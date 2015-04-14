ba730e45cb5d8d4ee7461f73d246683405c3fb8a
Mounir Ybanez <rinuom91@gmail.com>
2015-04-14 14:23:46 +0800

    Added insert and authenticate functions for the user.

--- model/user.go
+++ model/user.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+)
+
 type User struct {
 	Id			int		`db:"id"`
 	Username	string	`db:"username"`
@@ -7,4 +13,51 @@ type User struct {
 	LastName	string	`db:"last_name"`
 	Password	string	`db:"password"`
 	Email		string	`db:"email"`
+}
+
+func (u *User) Insert() bool {
+	var valid bool
+	db, _ := sql.Open("mysql", dbInfo)
+	var query = `
+		INSERT INTO users (username, first_name, last_name, password, email)
+		VALUES (?, ?, ?, ?, ?)
+	`
+	_, err := db.Exec(query, u.Username, u.FirstName, u.LastName, u.Password, u.Email)
+	if err != nil {
+		fmt.Print("error: ")
+		fmt.Println(err)
+		valid = false
+	} else {
+		valid = true
+	}
+	defer db.Close()
+
+	return valid
+}
+
+func (u *User) Authenticate() bool {
+	var valid bool
+	db, _ := sql.Open("mysql", dbInfo)
+	var query = `
+		SELECT COUNT(*) FROM users WHERE username=? AND password=?
+	`
+	rows, err := db.Query(query, u.Username, u.Password)
+	if err != nil {
+		fmt.Print("error: ")
+		fmt.Println(err)
+		valid = false
+	} else {
+		var count int
+		rows.Next()
+		rows.Scan(&count)
+		if count == 1 {
+			valid = true
+		} else {
+			valid = false
+		}
+		
+	}
+	defer db.Close()
+
+	return valid
 }
